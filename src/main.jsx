352371b7d50c6fac6757794ef25ac5e438ca4034
seitacos-ops <seitacos@gmail.com>
2026-06-06 13:21:28 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// StrictModeを外す（setStateコールバックの2重実行を防ぐ）
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
